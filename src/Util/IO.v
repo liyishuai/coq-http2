c08bde93bf11a7118601d232a9548da76c756e44
Lysxia <lysxia@gmail.com>
2018-09-17 13:31:59 -0400

    Little extraction example

---
+++ src/Util/IO.v
@@ -0,0 +1,29 @@
+From ExtLib.Structures Require Import Monad.
+
+From SimpleIO Require Import IOMonad CoqPervasives.
+
+From HTTP2.Util Require Import StringUtil Parser.
+
+Inductive file_parser (A : Type) : Type :=
+| FileParser : (in_channel -> IO A) -> file_parser A
+.
+
+Arguments FileParser {A}.
+
+Definition run_file_parser {A : Type} (fp : file_parser A) :
+  in_channel -> IO A :=
+  match fp with
+  | FileParser f => f
+  end.
+
+Instance Monad_file_parser : Monad file_parser := {
+  ret _ a := FileParser (fun _ => IOMonad.ret a);
+  bind _ _ m k := FileParser (fun h =>
+    IOMonad.bind (run_file_parser m h) (fun x =>
+    run_file_parser (k x) h));
+}.
+
+Instance MParser_file_parser : MParser byte file_parser := {
+  get_token := FileParser (fun h =>
+    IOMonad.map_io ascii_of_char (input_char h))
+}.
---
+++ test/io_roundtrip.v
@@ -0,0 +1,47 @@
+From Coq Require Import
+     NArith
+     Bool.Bvector.
+
+From SimpleIO Require Import
+     IOMonad CoqPervasives.
+
+From HTTP2 Require Import
+     Decode
+     Encode
+     Types
+     Util.BitVector
+     Util.ByteVector
+     Util.IO
+     Util.Parser.
+
+Require Extraction.
+
+Definition test_file := "/tmp/test_file".
+
+Import IONotations.
+
+Axiom Bvector_of_N : forall n, N -> Bvector n.
+Arguments Bvector_of_N {n}.
+
+Definition example_ping_frame : Frame := {|
+  frameHeader := {|
+    payloadLength := Bvector_of_N 8;
+    flags := Bvector_of_N 0;
+    streamId := Bvector_of_N 0;
+  |};
+  frameType := PingType;
+  framePayload := PingFrame (ByteVector_of_N _ 0);
+|}.
+
+Definition main : IO unit :=
+  out <- open_out test_file;;
+  output_string out (encodeFrame None example_ping_frame);;
+  close_out out;;
+  inp <- open_in test_file;;
+  fh <- run_file_parser (unindex decodeFrameHeader) inp;;
+  let '(ft, fh) := fh in
+  close_in inp.
+
+Definition exe := unsafe_run main.
+
+Extraction "io_roundtrip.ml" exe.
